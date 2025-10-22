65d1c5fc0dbe1bbd36c11bc95a5f4a0b8832008c
KalanaBimsara <kalanabimsara8@gmail.com>
2025-10-22 10:26:35 +0530

    Update OrderForm.tsx

--- src/pages/OrderForm.tsx
+++ src/pages/OrderForm.tsx
@@ -139,7 +139,7 @@ const OrderForm: React.FC = () => {
       <div className="form-copy" style={{ height: '50vh', pageBreakAfter: copyNumber % 2 === 0 ? 'always' : 'auto', pageBreakInside: 'avoid' }}>
         <div className="border-2 p-3 h-full" style={{ 
           fontFamily: 'Arial, sans-serif', 
-          fontSize: '12px',
+          fontSize: '50px',
           backgroundColor: colors.bg,
           borderColor: colors.border,
           color: colors.text
